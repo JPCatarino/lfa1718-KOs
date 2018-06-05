5a8e133e397489ac22d7742d84393bd33d48baa2
Daniel Marques <danielfmarques@ua.pt>
2018-06-05 14:40:03 +0100

    Merge branch 'master' of https://code.ua.pt/git/lfa-1718-g9
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- BaseGrammarSemanticCheck.java
+++ BaseGrammarSemanticCheck.java
@@ -3,7 +3,13 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class BaseGrammarSemanticCheck extends BaseGrammarBaseVisitor<Boolean> {
+
+
+    /*
+    @Override public Boolean visitCheckVar(BaseGrammarParser.CheckVarContext ctx){
+
     @Override public Boolean visitCommand(BaseGrammarParser.CommandContext ctx){
+
         Boolean res = true;
         String name = ctx.NAME().getText();
         if (!BaseGrammarParser.symbolTable.containsKey(name)){
@@ -12,23 +18,21 @@ public class BaseGrammarSemanticCheck extends BaseGrammarBaseVisitor<Boolean> {
         }
         return res;
     }
+    */
 
+    /*
     @Override public Boolean visitAssignment(BaseGrammarParser.AssignmentContext ctx){     
-        Boolean res = true;
-        String name = ctx.NAME().getText();
-        if (BaseGrammarParser.symbolTable.containsKey(name)){
-            ErrorHandling.printError(ctx, "Variable \""+name+"\" already declared!");
-            res = false;
-        }
-        else{
-            BaseGrammarParser.symbolTable.put(name, new VarAssign(name)); 
-        }
-        return res;
-    }
 
+    }
+    */
+
+    /*
     @Override public Boolean visitAssignVar(BaseGrammarParser.AssignVarContext ctx){     
         Boolean res = true;
         String name = ctx.NAME().getText();
+
+
+        ErrorHandling.printError(ctx, "Variable \""+name+"\" does not exist!");
         if (!BaseGrammarParser.symbolTable.containsKey(name)){
             ErrorHandling.printError(ctx, "Variable \""+name+"\" does not exist!");
             res = false;
@@ -49,10 +53,55 @@ public class BaseGrammarSemanticCheck extends BaseGrammarBaseVisitor<Boolean> {
     @Override public Boolean visitIncrement(BaseGrammarParser.IncrementContext ctx){     
         Boolean res = true;
         String name = ctx.NAME().getText();
+
         if (!BaseGrammarParser.symbolTable.containsKey(name)){
             ErrorHandling.printError(ctx, "Variable \""+name+"\" does not exist!");
             res = false;
         }
         return res;
     }
-}
+    */
+
+    /*
+    @Override public Boolean visitSimple(BaseGrammarParser.AssignVarContext ctx){
+        Boolean res = true;
+
+        String id = ctx.STRING().getText();
+
+        BGSymbol s = BaseGrammarParser.symbolTable.get(id);
+
+
+        if(s.type != vartype.simpVar){
+            ErrorHandling.printError(ctx, "Variable \"" + id + "\" should be Simple!");
+            res = false;
+
+        }
+        return res;
+    }
+    */
+    @Override public Boolean visitVarDec(BaseGrammarParser.AssignVarContext ctx){
+        Boolean res = true;
+        String id = ctx.NAME().getText();
+        if (BaseGrammarParser.symbolTable.containsKey(id)){
+            ErrorHandling.printError(ctx, "Variable \""+id+"\" already declared!");
+            res = false;
+        }
+        else{
+
+            if(ctx.varType().getText().equals("simpVar")){
+                BaseGrammarParser.symbolTable.put(id, new BaseGrammarSymbol(id, vartype.simpVar));
+
+            }
+
+
+        }
+        return res;
+
+
+    }
+
+
+
+
+
+    }
