5d89803f6f31b75b7ea42131f1e5c070b800193c
JPCatarino <jorge.catarino@ua.pt>
2018-06-24 20:05:46 +0100

    [Script] created a simple python script to help run project

---
+++ runBaseGrammar.py
@@ -0,0 +1,29 @@
+import subprocess
+import os.path
+
+def main():
+    filepath = input("file path: ")
+    if(filepath == ""):
+        compileAntlr()
+        compileJava()
+        exit()    
+    if(os.path.isfile(filepath)):
+        compileAntlr()
+        compileJava()
+        exeJava(filepath)
+        exit()
+    else:
+        print("no such file")
+        exit()
+
+
+def compileAntlr():
+    subprocess.Popen(["antlr4","-visitor","BaseGrammar.g4"]).wait()
+
+def compileJava():
+    subprocess.Popen("javac BaseGrammar*.java",shell=True).wait()
+
+def exeJava(filepath):
+    subprocess.Popen(["java","BaseGrammarMain",filepath]).wait()
+
+main()
